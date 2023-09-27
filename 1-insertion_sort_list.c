7d20cea7b94bc29063b4d2075c866b04e55fa6d5
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-09-27 20:50:59 +0300

    first commit for question 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list using the Insertion sort
+ * @list: list to sorted
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp = NULL, *node = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+	node = *list;
+	node = node->next;
+	while (node)
+	{
+		while (node->prev && node->n < (node->prev)->n)
+		{
+			temp = node;
+			if (node->next)
+			{
+				(node->next)->prev = temp->prev;
+			}
+			(node->prev)->next = temp->next;
+			node = node->prev;
+			temp->prev = node->prev;
+			temp->next = node;
+			if (node->prev)
+			{
+				(node->prev)->next = temp;
+			}
+			node->prev = temp;
+			if (temp->prev == NULL)
+			{
+				*list = temp;
+			}
+			print_list(*list);
+			node = node->prev;
+		}
+		node = node->next;
+	}
+}
